c6f76796207422fe7b84ff71684747144d646572
Nik <Nik@DESKTOP-7RH6ER1>
2021-12-06 00:52:48 +0300

    Program

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,24 +17,14 @@ namespace ConsoleApp
 
         private static async Task MainAsync()
         {
-            // Main body here
-            //string path = "C:\\Users\\Nik\\Desktop\\War and Peace\\txt\\TextFile.txt";
-            //string path = "C:\\Users\\Nik\\source\\repos\\TextAnalyzer\\ConsoleApp\\NewFolder\\TextFileExample8.txt";
-            string path = string.Empty;
-            while (!File.Exists(path))
-            {
-                Console.WriteLine("Enter the file path.");
-                path = Console.ReadLine();
-            }
+            //Get file path
+            string path = TryGetPathOrExit();
 
+            //Analyze file
             var watch = System.Diagnostics.Stopwatch.StartNew();
-
             DocOperator docOperator = new DocOperator();
-
             string[] lines = await docOperator.ReadAllLinesAsync(path, Encoding.UTF8);
-
             TextAnalyzer textAnalyzer = new TextAnalyzer();
-
             IEnumerable<KeyValuePair<string, int>> mostPopular = await textAnalyzer.GetMostPopularLetterLiteralsAsync(lines, 3, 10);
 
             //Display results
@@ -43,9 +33,39 @@ namespace ConsoleApp
                 Console.WriteLine("\"" + keyValuePair.Key + "\"" + " becomes " + keyValuePair.Value + " times");
             }
 
+            //Measure elapsed time
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
             Console.WriteLine("Elapsed " + elapsedMs + " milliseconds.");
+        }
+
+        private static string TryGetPathOrExit()
+        {
+            string path = string.Empty;
+            string message = "Enter the file path or press N for exit.";
+            while (!File.Exists(path))
+            {
+                Console.WriteLine(message);
+                path = Console.ReadLine();
+
+                if (path.ToLower() == "n")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (!File.Exists(path))
+                {
+                    message = "Incorrect file path. Enter the file path or press N for exit.";
+                    Console.WriteLine(message);
+                    path = Console.ReadLine();
+                    if (path.ToLower() == "n")
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            return path;
         }
     }
 }
